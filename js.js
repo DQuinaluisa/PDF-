40faee09a3cad3fa58a38e2371dc91b9aa00ab6f
DQuinaluisa <dft.quinaluisa@yavirac.edu.ec>
2020-01-14 23:24:18 -0500

    PDF Y OCULTAR TABLAS

---
+++ js.js
@@ -0,0 +1,40 @@
+function cartilla2 (){
+    document.getElementById('cartilla2').style.display='none';
+ 
+  }
+   function cartilla3 (){
+    document.getElementById('cartilla3').style.display='none';
+ 
+  }
+
+function cartilla4 (){
+    document.getElementById('cartilla4').style.display='none';
+ 
+  }
+
+  function esconderCartillas (){
+
+    cartilla2();
+    cartilla3();
+    cartilla4();
+
+  }
+
+    window.onload = esconderCartillas;
+
+
+  function mostrar2 (){
+    document.getElementById('cartilla2').style.display='block';
+  
+  }
+
+  function mostrar3 (){
+    document.getElementById('cartilla3').style.display='block';
+  
+  }
+
+  function mostrar4 (){
+    document.getElementById('cartilla4').style.display='block';
+  
+  }
+  
